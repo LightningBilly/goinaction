86b2307594313546154807befcb135e38713d907
chenbinbin <chenbinbin@didichuxing.com>
2019-05-11 15:41:44 +0800

    work

---
+++ chapter7/patterns/workimp/main/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "github.com/goinaction/code/chapter7/patterns/workimp"
+    "strconv"
+    "sync"
+)
+
+type Job struct {
+    Name string
+}
+
+func (j Job) Task() {
+    fmt.Println(j.Name)
+}
+
+func main() {
+    maxgo := 100
+
+    var wg sync.WaitGroup
+
+    wg.Add(maxgo)
+    pool := workimp.New(3)
+
+    for i:=0;i<100; i++ {
+        go func(id int) {
+            defer wg.Done()
+            s := strconv.FormatInt(int64(id), 10)
+            job := Job{s}
+            pool.Run(job)
+        }(i)
+    }
+    wg.Wait()
+    pool.ShutDown()
+}
+
+
---
+++ chapter7/patterns/workimp/work.go
@@ -0,0 +1,42 @@
+package workimp
+
+import (
+    "sync"
+    "fmt"
+)
+
+type Worker interface {
+    Task()
+}
+
+type Pool struct {
+    queue chan Worker
+    wg sync.WaitGroup
+}
+
+func (p *Pool)Run(w Worker) {
+    p.queue<-w
+}
+
+func (p *Pool)ShutDown() {
+    close(p.queue)
+    p.wg.Wait()
+}
+
+func New(poolSize int) *Pool {
+    pool := &Pool {
+        queue: make(chan Worker),
+    }
+
+    pool.wg.Add(poolSize)
+    for i:=0;i<poolSize; i++ {
+        go func(id int) {
+            defer pool.wg.Done()
+            for w:=range pool.queue {
+                fmt.Println("go ", id, "work")
+                w.Task()
+            }
+        }(i)
+    }
+    return pool
+}
